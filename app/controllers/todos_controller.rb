e46239464461893a02951ac78bc9378f878b47fe
Platform45-Nic <nicolo@platform45.com>
2019-09-25 00:54:42 +0200

    Added table to todo index view. Also added private create and destroy specific methods to todos_controller.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,7 +9,34 @@ class TodosController < ApplicationController
       } 
     )
     @todo_list = @response.find{|h| h}.select{|s,v| s == "items"}.to_a[0][1]
-    binding.pry
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def todos_params
+    params.permit(:email, :password)
+  end
+
+  def delete_todo
+    if logged_in = Session.find_by(id: session[:current_user_id])
+      logged_in.destroy
+      session.delete(:current_user_id)
+      @current_user_id = nil
+      redirect_to root_path
+    elsif logged_in = User.find_by(id: session[:current_user_id])
+      logged_in.destroy
+      session.delete(:current_user_id)
+      @current_user_id = nil
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
 end
