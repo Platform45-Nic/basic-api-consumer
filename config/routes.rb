a04bea520c188636bdb5a8783567cce5d10ca311
Platform45-Nic <nicolo@platform45.com>
2019-09-27 11:01:49 +0200

    Corrected routes to be grouped per controller. Moved auth_token into private instead of per method.

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,15 @@ Rails.application.routes.draw do
   root to: 'static_pages#index'
 
   get 'index', to: 'users#index'
+
+  resources :sessions, only: :create
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout',  to: 'sessions#destroy', as: 'logout'
+
+  resources :users, only:[:new, :create, :destroy]
+
+  resources :todos, only:[:show, :new, :create]
   get 'todos/index', to: 'todos#index', as: 'todo_index'
 
-  resources :sessions, only:[:new, :create]
-  resources :users, only:[:new, :create, :destroy]
-  resources :todos, only:[:show, :new, :create]
-
-  # post '/sessions', to: 'sessions#create'
-  # post '/sessions/new', to: 'sessions#new'
-  # get '/sessions/new', to: 'sessions#new'
-  # delete '/sessions/:id', to: 'sessions#destroy', as: 'session'
-
   get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  delete  '/logout',  to: 'sessions#destroy'
 end
